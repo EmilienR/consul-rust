72409b6273b22aa9fdd5aae842e9bd1ace57fb82
Emilien Richard <emilien@erichard.fr>
2023-03-10 13:03:55 +0100

    Added Meta, Weights and Namespace on services

--- tests/health.rs
+++ tests/health.rs
@@ -7,17 +7,46 @@ fn health_test() {
     let config = Config::new().unwrap();
     let client = Client::new(config);
     // An existing service for a agent in dev mode
-    let r = client
-        .service("consul", Option::None, true, Option::None)
-        .unwrap();
+    let r = client.service("consul", None, true, None).unwrap();
     let (snodes, meta) = (r.0, r.1);
     {
         assert!(!snodes.is_empty(), "should have at least one Service Node");
         assert!(meta.last_index.unwrap() > 0, "index must be positive");
+        if let Some(consul_srvc_entry) = snodes.get(0) {
+            if let Some(meta) = &consul_srvc_entry.Service.Meta {
+                assert!(
+                    meta.get("grpc_port").is_some(),
+                    "consul service meta grpc_port should exists"
+                );
+                assert_eq!(
+                    meta.get("grpc_port").unwrap(),
+                    "8502",
+                    "consul service meta grpc_port should be 8502"
+                );
+            } else {
+                panic!("consul service entry should have Meta values");
+            }
+            assert_eq!(
+                consul_srvc_entry.Service.Weights.get("Passing").unwrap(),
+                &1,
+                "consul service passing Weight must be 1"
+            );
+            assert_eq!(
+                consul_srvc_entry.Service.Weights.get("Warning").unwrap(),
+                &1,
+                "consul service warning Weight must be 1"
+            );
+            assert_eq!(
+                consul_srvc_entry.Service.Namespace, "",
+                "namespace should be empty for consul service"
+            );
+        } else {
+            panic!("No consul service entry found")
+        }
     }
     // A non existing, should be empty
     let r = client
-        .service("non-existing-service", Option::None, true, Option::None)
+        .service("non-existing-service", None, true, None)
         .unwrap();
     let (snodes, meta) = (r.0, r.1);
     {
